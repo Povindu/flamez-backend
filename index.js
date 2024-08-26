fd35fb1160ca13f1168d25adad97dd9c8ba575d8
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-08-26 13:40:09 +0530

    ci:

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require("express");
 const app = express();
 
 // set port, listen for requests
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
