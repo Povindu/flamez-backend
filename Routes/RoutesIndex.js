58a84b5fe65e0449d74f7155f3bf4289624ce95e
Povindu Samarasekara <povinduchanmith@gmail.com>
2024-10-04 11:21:45 +0530

    fix: service items authMiddleware changed

--- Routes/RoutesIndex.js
+++ Routes/RoutesIndex.js
@@ -5,18 +5,16 @@ const AuthMiddleware = require("../middleware/AuthMiddleware");
 
 const AuthRoutes = require("./AdminAuth.Routes");
 const ServiceItemRoutes = require("./ServiceItems.Routes");
+const TestaRoutes = require("./Testamonials.Routes");
 const ImageUploadRoutes = require("./ImageUpload.Routes");
 
-
 router.use("/auth", AuthRoutes);
-router.use("/services", AuthMiddleware, ServiceItemRoutes);
-router.use("/upload", ImageUploadRoutes, );
-
-
+router.use("/services", ServiceItemRoutes);
+router.use("/testa", TestaRoutes);
+router.use("/upload", AuthMiddleware, ImageUploadRoutes);
 
 router.get("/", (req, res) => {
   res.status(200).json({ message: "Flamez Test Endpoint v1" });
 });
-
 
 module.exports = router;
